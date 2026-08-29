31669c6b93669bade6a9303650b67ed86444ed0d
olivia-l0803 <olivialu415@gmail.com>
2026-08-29 15:27:55 -0400

    Commit

--- sunbeam/perfectDay/static/js/todo.js
+++ sunbeam/perfectDay/static/js/todo.js
@@ -1,9 +1,5 @@
-const buttons = document.getElementsByClassName('doneToggle');
 
-Array.from(buttons).forEach(button => { // for each button in buttons
-    button.addEventListener('click', () => { 
-        id = button.dataset
-        const inputValue = document.getElementById('username').value; 
-        console.log('User entered:', inputValue);
-  });
-});
+
+function markedDone(self){
+    id = self.dataset.id
+}
