4c0a921ac6a87bef62487784f8aac8646fca2189
olivia-l0803 <olivialu415@gmail.com>
2026-08-29 10:56:48 -0400

    HELP

--- sunbeam/perfectDay/models.py
+++ sunbeam/perfectDay/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class user(models.Model):
     username = models.CharField()
     password = models.CharField()
+
+
+    def __str__(self):
+        return self.username
