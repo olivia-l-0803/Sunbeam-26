888aea3d5b82cfb30f3140e3e154c627ede80dbb
olivia-l0803 <olivialu415@gmail.com>
2026-08-29 12:18:54 -0400

    login

--- sunbeam/perfectDay/urls.py
+++ sunbeam/perfectDay/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login, name='Login'),
     path('home/<int:userID>', views.homePage, name='homePage'),
     path('forum/<int:userID>', views.forum, name='Forum'),
 ]
--- sunbeam/perfectDay/views.py
+++ sunbeam/perfectDay/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, JsonResponse
+from .models import * 
 
 
+
+
+def login(request):
+
+    users= user.objects.all().values()
+
+    if request.method == 'POST':
+        inputUser = request.POST.get("usernamefield")
+        inputPass = request.POST.get("passwordfield")
+
+
+        #checking username
+        check = user.objects.filter(username = inputUser).exists()
+        if check == True:
+
+            #checking password
+            check2 = user.objects.filter(username = inputUser, password= inputPass).exists()
+            if check2 == True:
+                Id = (user.objects.get(username = inputUser, password= inputPass).id)
+                request.session['userID'] = Id
+                return redirect('homePage', Id ) #REDIRECTS USE THE NAME IN URLS
+            else:
+                messages.error(request = request,message="Your password is incorrect! Please try again.")
+
+                #space
+        else:
+            messages.error(request= request, message="This user does not exist! Please try again.")
+
+    Qcontext = {'users': users}
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render(request=request, context= Qcontext))
 
 def homePage(request, userID):
     data = {"ID": userID}
