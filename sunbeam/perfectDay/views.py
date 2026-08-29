4c5963336fe186c872228a6ed3d5f6ae773443e7
olivia-l0803 <olivialu415@gmail.com>
2026-08-29 10:17:17 -0400

    editing

--- sunbeam/perfectDay/views.py
+++ sunbeam/perfectDay/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
+def login(request):
+    data = {"x": "x"}
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render(context= data))
+
 def homePage(request):
-    data = {"fuck you": 2}
+    data = {"x": "x"}
     template = loader.get_template('homepage.html')
     return HttpResponse(template.render(context= data))
 
