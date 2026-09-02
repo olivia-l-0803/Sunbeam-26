a7307503cd7e5a508410433a153779bb377a4283
olivia-l0803 <olivialu415@gmail.com>
2026-09-01 22:53:20 -0400

    i created requirements.txt!

--- sunbeam/sunbeam/settings.py
+++ sunbeam/sunbeam/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-7j8qr50c0cwk$8042)6f$#)597ka4axx2z^6e-@7+!%oge*_o7')
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-7j8qr50c0cwk$8042)6f$#)597ka4axx2z^6e-@7+!%oge*_o7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = 'True'
+    #DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
