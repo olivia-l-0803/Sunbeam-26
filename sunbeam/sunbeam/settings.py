6c98e0a2f36a5fa6c063312c670f8c280c89575b
olivia-l0803 <olivialu415@gmail.com>
2026-09-04 22:00:03 -0400

    IT WORK IT WORK URETHRA

--- sunbeam/sunbeam/settings.py
+++ sunbeam/sunbeam/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'sunbean/static')
 
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
