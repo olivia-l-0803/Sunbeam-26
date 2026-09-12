87ae5baa722fe6f2ddd91135444d81fc09f905cf
olivia-l0803 <olivialu415@gmail.com>
2026-09-12 14:28:18 -0400

    Creating login page

--- sunbeam/build.sh
+++ sunbeam/build.sh
@@ -11,3 +11,6 @@ python manage.py migrate
 
 #TRANSFER DATA
 #python manage.py dumpdata --natural-foreign --natural-primary -e contenttypes -e auth.permission --indent 4 -o datadump.json
+
+#Run development server
+#python -m uvicorn sunbeam.asgi:application --host 0.0.0.0 --port 8000 
